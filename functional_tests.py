376b6a9bfe7b3cbbea928d9c27d9e7d5ee2e3e73
Nikolay <azrielle@mail.ru>
2019-10-22 18:30:35 +0300

    Обработка POST и вывод в шаблон HTML 1 элемента списка

--- functional_tests.py
+++ functional_tests.py
@@ -43,20 +43,35 @@ class NewVisitorTest(unittest.TestCase):
 
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
-		self.assertTrue(
-			any(row.text == '1: Купить павлиные перья' for row in  rows),
-			"Новый элемент списка не появился в таблице"
-		)
+		self.assertIn('1: Купить павлиные перья', [row.text for row in rows])
 
 		# Текстовое поле по-прежнему приглашает ее добавить еще один элемент.
 		# Она вводит "Сделать мушку из павлиньих перьев"
-		
-		self.fail('Закончить тест!')
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Сделать мушку из павлиньих перьев')
+		inputbox.send_keys(Keys.ENTER)
+		time.sleep(1)
+
 		# Страница снова обновляется,и теперь показывает оба элемента ее списка.
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn('1: Купить павлиные перья', [row.text for row in rows])
+		self.assertIn(
+			'2: Сделать мушку из павлиньих перьев', 
+			[row.text for row in rows]
+		)
+
+
+
+
+
+		
+		
 
 		# Эдит интересно, запомнит ли сайт ее список. Далее она видит, что
 		# сайт сгенерировал для нее уникальный URL-адрес - об этом 
 		# выводится небольшой текст с обьяснениями.
+		self.fail('Закончить тест!')
 
 		# Она посещает этот URL-адрес - ее список по-прежнему там.
 
