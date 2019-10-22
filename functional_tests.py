190226a7ff5192b553a743f09e5634ae60c02f6f
Nikolay <azrielle@mail.ru>
2019-10-22 17:05:13 +0300

    HTML главной страницы теперь генерируется из шаблона

--- functional_tests.py
+++ functional_tests.py
@@ -23,12 +23,12 @@ class NewVisitorTest(unittest.TestCase):
 		# Она видит, что заголовок и шапка страницы говорят о списках
 		# неотложных дел
 		self.assertIn('To-Do',self.browser.title)
-		header_text = self.browser.find_element_by_tag_name('h1').header_text
+		header_text = self.browser.find_element_by_tag_name('h1').text
 		self.assertIn('To-Do',header_text)
 
 		# Ей сразу же предлогается ввести элементт списка
 		inputbox = self.browser.find_element_by_id('id_new_item')
-		self.asserEqual(
+		self.assertEqual(
 			inputbox.get_attribute('placeholder'),
 			'Enter a to-do item'
 		)
@@ -41,10 +41,11 @@ class NewVisitorTest(unittest.TestCase):
 		inputbox.send_keys(Keys.ENTER)
 		time.sleep(1)
 
-		table = self.browser.find_element_by_id('id_last_table')
+		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
-		self.asserTrue(
-			any(row.text == '1: Купить павлиные перья' for row in  rows)
+		self.assertTrue(
+			any(row.text == '1: Купить павлиные перья' for row in  rows),
+			"Новый элемент списка не появился в таблице"
 		)
 
 		# Текстовое поле по-прежнему приглашает ее добавить еще один элемент.
