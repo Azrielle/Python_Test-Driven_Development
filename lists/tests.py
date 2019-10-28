62a069fcb650748587507a64594d579f988af75e
Azrielle <azrielllle@gmail.com>
2019-10-28 17:44:19 +0300

    Новый URL + представление для добавления в существующиу списки. ФТ проходит :-)

--- lists/tests.py
+++ lists/tests.py
@@ -27,7 +27,8 @@ class NewListTest(TestCase):
 	def test_redirects_after_POST(self):
 		'''тест: переадресует после post-запроса'''
 		response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
-		self.assertRedirects(response, '/lists/one-list-world/')
+		new_list = List.objects.first()
+		self.assertRedirects(response, f'/lists/{new_list.id}/')
 
 
 
@@ -36,20 +37,33 @@ class ListViewTest( TestCase ):
 
 	def test_uses_list_template(self):
 		'''тест: используется шаблон списка'''
-		response = self.client.get('/lists/one-list-world/')
+		list_ = List.objects.create()
+		response = self.client.get(f'/lists/{list_.id}/')
 		self.assertTemplateUsed(response, 'list.html')
 
-	def test_display_all_items(self):
-		'''тест: отображаются все элементы списка'''
-		list_ = List.objects.create()
-		Item.objects.create(text='itemey 1', list=list_)
-		Item.objects.create(text='itemey 2', list=list_)
+	def test_displays_only_items_for_that_list(self):
+		'''тест: отображаются элементы только для этого списка'''
+		correct_list = List.objects.create()
+		Item.objects.create(text='itemey 1', list=correct_list)
+		Item.objects.create(text='itemey 2', list=correct_list)
+		other_list = List.objects.create()
+		Item.objects.create(text='другой элемент 1 списка', list=other_list)
+		Item.objects.create(text='другой элемент 2 списка', list=other_list)
 
-		response = self.client.get('/lists/one-list-world/')
+
+		response = self.client.get(f'/lists/{correct_list.id}/')
 
 		self.assertContains(response, 'itemey 1')
 		self.assertContains(response, 'itemey 2')
+		self.assertNotContains(response, 'другой элемент 1 списка')
+		self.assertNotContains(response, 'другой элемент 2 списка')
 
+	def test_passes_correct_list_to_tamplate(self):
+		'''тест: представляется правильный шаблон списка'''
+		other_list = List.objects.create()
+		correct_list = List.objects.create()
+		response = self.client.get(f'/lists/{correct_list.id}/')
+		self.assertEqual(response.context['list'], correct_list)
 
 class ListAndItemModelsTest(TestCase):
 	'''тест модели элемента списка '''
@@ -82,3 +96,34 @@ class ListAndItemModelsTest(TestCase):
 		self.assertEqual(first_saved_item.list, list_)
 		self.assertEqual(second_saved_item.text, 'Item the second')
 		self.assertEqual(second_saved_item.list, list_)
+
+class NewItemTest(TestCase):
+	'''тест нового элемента списка'''
+
+	def test_can_save_a_POST_request_to_an_existing_list(self):
+		'''тест: можно сохранить POST-запрос в существуещем список'''
+		other_list = List.objects.create()
+		correct_list = List.objects.create()
+
+		self.client.post(
+			f'/lists/{correct_list.id}/add_item',
+			data={'item_text': 'A new item for an existing list'}
+		)
+
+		self.assertEqual(Item.objects.count(), 1)
+		new_item = Item.objects.first()
+		self.assertEqual(new_item.text, 'A new item for an existing list')
+		self.assertEqual(new_item.list, correct_list)
+
+	def test_redirects_to_list_view(self):
+		'''тест: переадресуется в представление списка'''
+		other_list = List.objects.create()
+		correct_list = List.objects.create()
+
+		response = self.client.post(
+			f'/lists/{correct_list.id}/add_item',
+			data={'item_text': 'A new item for an existing list'}
+		)
+
+
+		self.assertRedirects(response, f'/lists/{correct_list.id}/')
--- lists/views.py
+++ lists/views.py
@@ -6,14 +6,21 @@ def home_page(request):
 	'''домашняя страница'''
 	return render(request, 'home.html')
 
-def view_list(request):
+def view_list(request, list_id):
 	'''представление списка'''
-	items = Item.objects.all()
+	list_ = List.objects.get(id=list_id)
 
-	return render(request, 'list.html',{'items': items})
+	return render(request, 'list.html', {'list': list_})
 
 def new_list(request):
 	''' новый список '''
 	list_ = List.objects.create()
 	Item.objects.create(text=request.POST['item_text'], list=list_)
-	return redirect('/lists/one-list-world/')
+	return redirect(f'/lists/{list_.id}/')
+
+def add_item(request, list_id):
+	'''добавить элемент'''
+	list_ = List.objects.get(id=list_id)
+	Item.objects.create(text=request.POST['item_text'], list=list_)
+
+	return redirect(f'/lists/{list_.id}/')
