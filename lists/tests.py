6d26e2c386c260116c87de964256646d16c7a3b5
Azrielle <azrielllle@gmail.com>
2019-10-28 14:27:35 +0300

    URL-адреса для 2 форм, перемещен код в представления,+ тесты, новый URL-адрем

--- lists/tests.py
+++ lists/tests.py
@@ -12,9 +12,13 @@ class HomePageTest(TestCase):
 		self.assertTemplateUsed(response, 'home.html')
 
 
+
+class NewListTest(TestCase):
+	''' тест нового списка'''
+
 	def test_can_save_a_POST_request(self):
 		''' тест: можно сохранить post-запрос'''
-		self.client.post('', data={'item_text': 'A new list item'})
+		self.client.post('/lists/new', data={'item_text': 'A new list item'})
 		self.assertEqual(Item.objects.count(), 1)
 		new_item = Item.objects.first()
 		self.assertEqual(new_item.text, 'A new list item')
@@ -22,15 +26,10 @@ class HomePageTest(TestCase):
 
 	def test_redirects_after_POST(self):
 		'''тест: переадресует после post-запроса'''
-		response = self.client.post('', data={'item_text': 'A new list item'})
-		self.assertEqual(response.status_code, 302)
-		self.assertEqual(response['location'], '/lists/one-list-world/')
+		response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+		self.assertRedirects(response, '/lists/one-list-world/')
 
 
-	def test_only_saves_items_when_necessary(self):
-		'''тест: сохроняет элементы, только когда нужно'''
-		self.client.get('/')
-		self.assertEqual(Item.objects.count(), 0)
 
 class ListViewTest( TestCase ):
 	'''тест представление списка'''
--- lists/views.py
+++ lists/views.py
@@ -4,10 +4,6 @@ from lists.models import Item
 # Create your views here.
 def home_page(request):
 	'''домашняя страница'''
-	if request.method == 'POST':
-		Item.objects.create(text=request.POST['item_text'])
-		return redirect('/lists/one-list-world/')
-
 	return render(request, 'home.html')
 
 def view_list(request):
@@ -15,3 +11,8 @@ def view_list(request):
 	items = Item.objects.all()
 
 	return render(request, 'list.html',{'items': items})
+
+def new_list(request):
+	''' новый список '''
+	Item.objects.create(text=request.POST['item_text'])
+	return redirect('/lists/one-list-world/')
