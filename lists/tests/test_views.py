425f36d7b32a74b53e9cded35c2869e6a352fbb8
Azrielle <azrielllle@gmail.com>
2019-11-06 16:32:59 +0300

    Рефакторизовано представление списка для обработки POST-запросов с новыми элементами

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -79,17 +79,13 @@ class ListViewTest( TestCase ):
 		response = self.client.get(f'/lists/{correct_list.id}/')
 		self.assertEqual(response.context['list'], correct_list)
 
-
-class NewItemTest(TestCase):
-	'''тест нового элемента списка'''
-
 	def test_can_save_a_POST_request_to_an_existing_list(self):
 		'''тест: можно сохранить POST-запрос в существуещем список'''
 		other_list = List.objects.create()
 		correct_list = List.objects.create()
 
 		self.client.post(
-			f'/lists/{correct_list.id}/add_item',
+			f'/lists/{correct_list.id}/',
 			data={'item_text': 'A new item for an existing list'}
 		)
 
@@ -98,15 +94,17 @@ class NewItemTest(TestCase):
 		self.assertEqual(new_item.text, 'A new item for an existing list')
 		self.assertEqual(new_item.list, correct_list)
 
-	def test_redirects_to_list_view(self):
+	def test_POST_redirects_to_list_view(self):
 		'''тест: переадресуется в представление списка'''
 		other_list = List.objects.create()
 		correct_list = List.objects.create()
 
 		response = self.client.post(
-			f'/lists/{correct_list.id}/add_item',
+			f'/lists/{correct_list.id}/',
 			data={'item_text': 'A new item for an existing list'}
 		)
 
 
 		self.assertRedirects(response, f'/lists/{correct_list.id}/')
+
+
