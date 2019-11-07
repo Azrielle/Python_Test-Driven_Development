709a7a75ee99f1c8bc3435ce958d274ccf789bf5
Azrielle <azrielllle@gmail.com>
2019-11-07 12:43:47 +0300

    Применена get_absolute_url к модели списка для удаления url в представления (привало DRY)

--- lists/models.py
+++ lists/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 # Create your models here.
 class List(models.Model):
 	''' список '''
-	pass
+	def get_absolute_url(self):
+		'''получить абсолютный url'''
+		return reverse('view_list', args=[self.id])
 
 class Item(models.Model):
 	'''элемент списка'''
--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -43,3 +43,8 @@ class ListAndItemModelsTest(TestCase):
 			item.save()
 			item.full_clean()
 
+
+	def test_get_absolute_url(self):
+		'''тест: получение абсолютный url'''
+		list_ = List.objects.create()
+		self.assertEqual(list_.get_absolute_url(), f'/lists/{list_.id}/')
