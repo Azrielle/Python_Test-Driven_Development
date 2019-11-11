a0bf6df0da26b1192f406f4646eb6ad289c898b3
Azrielle <azrielllle@gmail.com>
2019-11-11 13:45:33 +0300

    Реализована валидация наличия повторяющегося элемента на уровне модели

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -46,3 +46,21 @@ class ItemValidationTest(FunctionalTest):
 		self.wait_for_row_in_list_table('1: Buy milk')
 		self.wait_for_row_in_list_table('2: Make tea')
 
+	def test_cannot_add_duplicate_items(self):
+		'''тест: нельзя добовлять повторяющиеся элементы'''
+		# Эдит открывает домашнюю страницу и начинает новый список
+		self.browser.get(self.live_server_url)
+		self.get_item_input_box().send_keys('Buy wellies')
+		self.get_item_input_box().send_keys(Keys.ENTER)
+		self.wait_for_row_in_list_table('1: Buy wellies')
+
+		# Она случайно пытается ввести повторяющийся элемент
+		self.get_item_input_box().send_keys('Buy wellies')
+		self.get_item_input_box().send_keys(Keys.ENTER)
+
+		# Она видит полезное сообщение об ошибке
+		self.wait_for(lambda: self.assertEqual(
+			self.browser.find_element_by_css_selector('.has-error').text,
+			"You've already go this in your list"
+		))
+
--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from lists.forms import EMPTY_ITEM_ERROR, ItemForm
+from lists.models import Item, List
 
 class ItemFormTest(TestCase):
 	'''тест формы для элемента списка'''
@@ -15,4 +16,13 @@ class ItemFormTest(TestCase):
 		form = ItemForm(data={'text': ''})
 		self.assertFalse(form.is_valid())
 		self.assertEqual(form.errors['text'], [EMPTY_ITEM_ERROR])
+		
+	def test_form_save_handles_saving_to_a_list(self):
+		'''тест: метод save формы обрабатывает сохранение в список'''
+		list_ = List.objects.create()
+		form = ItemForm(data={'text': 'do me'})
+		new_item = form.save(for_list=list_)
+		self.assertEqual(new_item, Item.objects.first())
+		self.assertEqual(new_item.text, 'do me')
+		self.assertEqual(new_item.list, list_)
 		
