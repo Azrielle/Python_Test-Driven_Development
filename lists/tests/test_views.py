ebf0aa7cf61b445ae9f9ec941019aab3933a746c
Azrielle <azrielllle@gmail.com>
2019-11-11 15:30:08 +0300

    класс ExistingListItemForm

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -2,6 +2,7 @@ from django.utils.html import escape
 from django.test import TestCase
 from lists.models import Item, List
 from lists.forms import ItemForm, EMPTY_ITEM_ERROR
+from unittest import skip
 
 # Create your tests here.
 class HomePageTest(TestCase):
@@ -148,6 +149,22 @@ class ListViewTest( TestCase ):
 		'''тест на недопустимый ввод: на странице показывается ошибка'''
 		response = self.post_incalid_input()
 		self.assertContains(response, escape(EMPTY_ITEM_ERROR))
+
+	@skip
+	def test_dublicate_item_validation_errors_end_up_on_lists_page(self):
+		'''тест: ошибки валидации повторяющегося элемента
+		   оканчиваются на странице списка'''
+		list1 = List.objects.create()
+		item1 = Item.objects.create(list=list1, text='textey')
+		response = self.client.post(
+			f'/lists/{list1.id}/',
+			data={'text': 'textey'}
+		)
+
+		expected_error = escape("You've already got this in your list")
+		self.assertContains(response, expected_error)
+		self.assertTemplateUsed(response, 'list.html')
+		self.assertEqual(Item.objects.all().count(), 1)
 
 	def test_displays_item_form(self):
 		'''тест отображения формы для элемента'''
