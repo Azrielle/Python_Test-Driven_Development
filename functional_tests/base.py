b140254e5b11493488d399b0b3f58311f1081a58
Azrielle <azrielllle@gmail.com>
2019-11-15 11:33:13 +0300

    улучшена функция в base.py добавлен декоратор wait

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -20,47 +20,43 @@ class FunctionalTest(StaticLiveServerTestCase):
 		'''демонтаж'''
 		self.browser.quit()
 
+	def wait(fn):
+		def modified_fn(*args, **kwargs):
+			start_time = time.time()
+			while True:
+				try:
+					return fn(*args, **kwargs)
+				except (AssertionError, WebDriverException) as e:
+					if time.time() - start_time > MAX_WAIT:
+						raise e
+					time.sleep(0.5)
+		return modified_fn
+
+	@wait
 	def wait_for_row_in_list_table(self,row_text):
 		'''подтверждение строки в таблице списка'''
-		start_time = time.time()
-		while True:
-			try:
-				table = self.browser.find_element_by_id('id_list_table')
-				rows = table.find_elements_by_tag_name('tr')
-				self.assertIn(row_text, [row.text for row in rows])
-				return
-			except (AssertionError, WebDriverException) as e:
-				if time.time() - start_time > MAX_WAIT:
-					raise e
-				time.sleep(0.5)
-
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
+	
+	@wait	
 	def wait_for(self,fn):
-		'''ожидать'''
-		start_time = time.time()
-		while True:
-			try:
-				return fn()
-			except (AssertionError, WebDriverException) as e:
-				if time.time() - start_time > MAX_WAIT:
-					raise e
-				time.sleep(0.5)
+		return fn()
 
 	def get_item_input_box(self):
 		'''получить поле ввода для элемента'''
 		return self.browser.find_element_by_id('id_text')
 
+	@wait
 	def wait_to_be_logged_in(self, email):
 		'''ожидать входа в систему'''
-		self.wait_for(
-			lambda: self.browser.find_element_by_link_text('Log out')
-		)
+		self.browser.find_element_by_link_text('Log out')
 		navbar = self.browser.find_element_by_css_selector('.navbar')
 		self.assertIn(email, navbar.text)
 
+	@wait
 	def wait_to_be_logged_out(self, email):
 		'''ожидать выхода из системы'''
-		self.wait_for(
-			lambda: self.browser.find_element_by_name('email')
-		)
+		self.browser.find_element_by_name('email')
 		navbar = self.browser.find_element_by_css_selector('.navbar')
 		self.assertNotIn(email, navbar.text)
