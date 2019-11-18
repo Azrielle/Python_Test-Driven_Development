e4bca0ab32c02a36d7374f3460236c7aff7db2b5
Azrielle <azrielllle@gmail.com>
2019-11-18 17:57:31 +0300

    Проходят все ФТ конец главы 22

--- lists/models.py
+++ lists/models.py
@@ -9,6 +9,11 @@ class List(models.Model):
 	def get_absolute_url(self):
 		'''получить абсолютный url'''
 		return reverse('view_list', args=[self.id])
+	
+	@property
+	def name(self):
+		'''имя'''
+		return self.item_set.first().text
 
 class Item(models.Model):
 	'''элемент списка'''
@@ -21,4 +26,6 @@ class Item(models.Model):
 
 	def __str__(self):
 		return self.text
+
+	
 
--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -81,3 +81,10 @@ class ListModelsTest(TestCase):
 	def test_list_owner_is_optional(self):
 		'''тест: владелец списка является необязательным'''
 		List.objects.create() # не должно поднять исключение
+	
+	def test_list_name_is_first_item_text(self):
+		'''тест: имя списка является текстом первого элемента'''
+		list_ = List.objects.create()
+		Item.objects.create(list=list_, text='first item')
+		Item.objects.create(list=list_, text='second item')
+		self.assertEqual(list_.name, 'first item')
